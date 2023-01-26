99e27de592b8a6557c2d10ec7656d9abc4b28002
nicholas hung <nhung93@outlook.com>
2023-01-25 20:25:08 -0500

    don't reload dataset when searching + more jp vocab

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,7 @@
   
   let selected: string = "chinese";
   let data: Record<string, string>[] = [];
+  let filteredData: Record<string, string>[] = [];
   let currCardIndex: number = 0;
   let totalCards: number = data.length;
   
@@ -22,6 +23,7 @@
       const d = await res.json();
       
       data = mapper.processChineseDataset(d);
+      filteredData = data;
       totalCards = data.length;
   });
   
@@ -44,20 +46,10 @@
       
       currCardIndex = 0;
       totalCards = data.length;
+      filteredData = data;
   }
   
   const onChangeSearch = async () => {
-      const mapper = new Mapper();
-      const res = await fetch(`datasets/${selected}.json`);
-      const d = await res.json();
-      
-      let currData: Record<string, string>[];
-      if(selected === "chinese"){
-        currData = mapper.processChineseDataset(d);
-      }else if(selected === "japanese"){
-        currData = mapper.processJapaneseDataset(d);
-      }
-      
       const searchInput: HTMLInputElement | null = document.querySelector('.searchInput');
       if(searchInput){
           const inputVal = searchInput.value;
@@ -67,14 +59,15 @@
             if(selectedRadioBtn){
                 // TODO: create type here?
                 const selectedSide: string = selectedRadioBtn.value;
-                currData = currData.filter(x => (x[selectedSide]).includes(inputVal));
+                filteredData = data.filter(x => (x[selectedSide]).includes(inputVal));
             }
+          }else{
+            filteredData = data;
           }
       }
       
       currCardIndex = 0;
-      data = currData;
-      totalCards = data.length;
+      totalCards = filteredData.length;
   }
   
 </script>
@@ -85,7 +78,7 @@
 
 <main>
   <div class="options-panel {showOptionsPanel ? 'options-panel-on' : 'options-panel-off'}">
-    <p>dataset: &nbsp;</p>
+    <p>dataset: </p>
     <select bind:value={selected} on:change={onChange}>
         {#each datasets as ds}
             <option value={ds}>{ds}</option>
@@ -94,7 +87,6 @@
     <p> | </p>
     <p> search: </p>
     <input class="searchInput" type="text" name="search" on:input={onChangeSearch}>
-    <p> | </p>
     <input type="radio" id="search-front-choice" name="search-side-choice" value="front" on:change={onChangeSearch}>
     <label for="search-front-choice">front</label>
     <input type="radio" id="search-back-choice" name="search-side-choice" value="back" on:change={onChangeSearch}>
@@ -107,7 +99,7 @@
   
   <div class='cardContainer'>
     {#if totalCards > 0}
-        <Card frontData={data[currCardIndex].front} backData={data[currCardIndex].back} />
+        <Card frontData={filteredData[currCardIndex].front} backData={filteredData[currCardIndex].back} />
     {/if}
   </div>
   

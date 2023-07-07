bbcedf193932841853854e014ab9ad076df467c0
nicholas hung <nhung93@outlook.com>
2023-07-06 20:42:14 -0400

    fix json + add json validator

---
+++ validate_json.js
@@ -0,0 +1,24 @@
+// validate the JSON datasets
+import { readFileSync } from 'fs';
+
+function checkDatasets(){
+    const datasets = [
+        "public/datasets/chinese.json",
+        "public/datasets/japanese.json",
+    ];
+
+    datasets.forEach(dataset => {
+        const json = readFileSync(dataset);
+        try {
+            JSON.parse(json);
+        } catch(err) {
+            console.log(`${dataset} is invalid JSON. error: ${err}`);
+            process.exit(1);
+        }
+    });
+    
+    process.exit(0);
+}
+
+checkDatasets();
+
